574fbad1e7dfae9dc9b6978bb519a941f189641d
Mikhail <mizolotu@jyu.fi>
2021-11-16 10:38:26 +0200

    16.11.2021

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ use_inputs = True
 use_outputs = True
 action_scale = 1
 wp_size = 1
-lookback = 256
+lookback = 512
 tstep = 0.01
 bonus = 10
 nwaypoints = 16
@@ -30,7 +30,7 @@ validation_size = 0.1
 ppo_net_arch = [
     #('conv1d', 256, 10, 5, 'valid'), ('conv1d', 512, 10, 5, 'valid'), ('dense', 1024),
     #('conv1d', 256, 10, 5, 'valid'), ('lstm', 256, False), ('dense', 512),
-    ('lstm', 64, True), ('lstm', 64, False), ('dense', 64),
-    #('lstm', 64, True), ('dense', 64),
+    #('lstm', 64, True), ('lstm', 64, False), ('dense', 64),
+    ('lstm', 64, True), ('dense', 64),
     dict(vf=[64, 64]), dict(pi=[64, 64])
 ]
